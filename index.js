44d1d5155e348520ed1310698a99b43fd7fe9d91
Martinho Macedo <martinhomacedo111@gmail.com>
2024-08-15 19:13:14 +0100

    3.18

--- index.js
+++ index.js
@@ -16,35 +16,11 @@ morgan.token('content', (req, res) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
   const date = new Date()
-  response.send(`
-Phonebook has info for ${persons.length} people <br/>
-${date}
-  `)
+  Person.find({}).then(persons =>
+    response.send(`Phonebook has info for ${persons.length} people <br/>${date}`)
+  ).catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
