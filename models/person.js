6e7114bf2bb83dcd624de63d8d074fb92df18216
Martinho Macedo <martinhomacedo111@gmail.com>
2024-08-16 12:02:30 +0100

    3.19

--- models/person.js
+++ models/person.js
@@ -21,7 +21,14 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: v => /\d{2}-\d*/.test(v) || /\d{3}-\d*/.test(v),
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
